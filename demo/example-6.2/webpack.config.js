3e28d0c8a1d42450a15cd6abd89add21fe37f5fe
lvzhenbang <lvzhenbang@outlook.com>
2019-01-13 00:25:57 +0800

    update: webpack-dev-server

---
+++ demo/example-6.2/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './main.js'
+    },
+    devServer: {
+        open: true
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+           {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo',
+            template: './template/index.html'
+        })
+    ]
+}
